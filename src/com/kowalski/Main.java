8a9c3fae9bfae27b2b99b75979f4707d6a9f0875
Egor Laptev <laptencio@yandex.ru>
2022-11-08 02:03:54 +0400

    Program works

--- src/com/kowalski/Main.java
+++ src/com/kowalski/Main.java
@@ -12,36 +12,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         List<String> products = Arrays.asList("Хлеб", "Сок", "Мясо", "Молоко",
-                "Сыр", "Шоколад", "Чипсы", "Макароны", "Яйца", "Орехи");
+                "Сыр", "Шоколад", "Чипсы", "Макароны", "Яйца", "Орехи", "Хлебные сушки", "Хлебные палочки");
+
         List<String> prodList = new ArrayList<>();
 
-
         while (true){
-
             System.out.println("");
-            System.out.println("Выберите операцию: \n1. Добавить\n2. Показать\n3. Удалить \n4. Выход");
+            System.out.println("Выберите операцию: \n1. Добавить\n2. Показать\n3. Удалить \n4. Поиск \n5. Выход");
 
             try {
             String inputOperation = scanner.nextLine();
             int inputOperationInt = Integer.parseInt(inputOperation);
 
-            if (inputOperationInt <0 || inputOperationInt >4){
-                System.out.println("incorrect input");
+            if (inputOperationInt <0 || inputOperationInt >5){
+                System.out.println("Incorrect input");
             }
-
                 if (inputOperationInt == 1) {
                     System.out.println("Какую покупку хотите добавить?");
-                    for (int i = 0; i < products.size(); i++) {
-                        System.out.println((i + 1) + ". " + products.get(i));
-                    }
+                    prodList(products);
+
                     try {
                     int inputAddProduct = Integer.parseInt(scanner.nextLine());
+
                         if (inputAddProduct < 0 || inputAddProduct > products.size()) {
-                            System.out.println("incorrect input");
+                            System.out.println("Incorrect input");
                         } else {
                             prodList.add(products.get(inputAddProduct - 1));
                             System.out.println("Итого в списке покупок: " + prodList.size());
-                            System.out.println("Dobavlena" + prodList);
                             continue;
                         }
                     } catch (NumberFormatException e) {
@@ -51,26 +48,42 @@ public class Main {
 
             if (inputOperationInt == 2){
                 System.out.println("Список покупок:");
-                for (int i = 0; i < prodList.size(); i++) {
-                    System.out.println((i +1) +". " + prodList.get(i));
-                }
+                prodList(prodList);
 
             } else if (inputOperationInt == 3){
                 System.out.println("Какую покупку хотите удалить? Введите номер или название");
+                prodList(prodList);
+                String inputDeleteProduct = scanner.nextLine();
+
+                if (prodList.remove(inputDeleteProduct)) {
+                    prodList.remove(inputDeleteProduct);
+                    System.out.println("Итого в списке покупок: " + prodList.size());
+                } else if (Integer.parseInt(inputDeleteProduct) > 0 && Integer.parseInt(inputDeleteProduct) < prodList.size()){
+                    prodList.remove(Integer.parseInt(inputDeleteProduct) -1);
+                    System.out.println("Итого в списке покупок: " + prodList.size());
+                } else {
+                    System.out.println("Incorrect input");
+                }
 
             } else if (inputOperationInt == 4){
+                System.out.println("Введите текст для поиска: ");
+                String inputSearchProduct = scanner.nextLine();
+                products.stream()
+                        .filter(searchProduct -> searchProduct.toLowerCase().contains(inputSearchProduct.toLowerCase()))
+                        .forEach(searchProduct -> System.out.println((products.indexOf(searchProduct) +1) + ". " + searchProduct));
+
+            } else if (inputOperationInt == 5){
                 break;
             }
-
             } catch (NumberFormatException e) {
-                System.out.println("Не корректный ввод! Введите число от 1 до 4");
+                System.out.println("Incorrect input");
             }
-
-
-
-
-
         }
+    }
 
+    public static <T> void prodList(List<T> list){
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println((i + 1) + ". " + list.get(i));
+        }
     }
 }
