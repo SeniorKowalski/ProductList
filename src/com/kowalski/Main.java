35fb42a4d291bb92220321d457ccce60edb0cd3e
Egor Laptev <laptencio@yandex.ru>
2022-11-08 20:32:51 +0400

    Refactor code and remake add-function

--- src/com/kowalski/Main.java
+++ src/com/kowalski/Main.java
@@ -1,7 +1,6 @@
 package com.kowalski;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,16 +9,11 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        List<String> products = Arrays.asList("Хлеб", "Сок", "Мясо", "Молоко",
-                "Сыр", "Шоколад", "Чипсы", "Макароны", "Яйца", "Орехи", "Хлебные сушки", "Хлебные палочки");
-
         List<String> prodList = new ArrayList<>();
 
         while (true){
             System.out.println("");
             System.out.println("Выберите операцию: \n1. Добавить\n2. Показать\n3. Удалить \n4. Поиск \n5. Выход");
-
             try {
             String inputOperation = scanner.nextLine();
             int inputOperationInt = Integer.parseInt(inputOperation);
@@ -27,58 +21,61 @@ public class Main {
             if (inputOperationInt <0 || inputOperationInt >5){
                 System.out.println("Incorrect input");
             }
-                if (inputOperationInt == 1) {
-                    System.out.println("Какую покупку хотите добавить?");
-                    prodList(products);
+                if (addProduct(scanner, prodList, inputOperationInt)) continue;
 
-                    try {
-                    int inputAddProduct = Integer.parseInt(scanner.nextLine());
-
-                        if (inputAddProduct < 0 || inputAddProduct > products.size()) {
-                            System.out.println("Incorrect input");
-                        } else {
-                            prodList.add(products.get(inputAddProduct - 1));
-                            System.out.println("Итого в списке покупок: " + prodList.size());
-                            continue;
-                        }
-                    } catch (NumberFormatException e) {
-                        System.out.println("Не корректный ввод! Введите число от 1 до "+ products.size());
-                    }
-                }
-
-            if (inputOperationInt == 2){
+                if (inputOperationInt == 2){
                 System.out.println("Список покупок:");
                 prodList(prodList);
 
             } else if (inputOperationInt == 3){
-                System.out.println("Какую покупку хотите удалить? Введите номер или название");
-                prodList(prodList);
-                String inputDeleteProduct = scanner.nextLine();
+                    deleteProduct(scanner, prodList);
 
-                if (prodList.remove(inputDeleteProduct)) {
-                    prodList.remove(inputDeleteProduct);
-                    System.out.println("Итого в списке покупок: " + prodList.size());
-                } else if (Integer.parseInt(inputDeleteProduct) > 0 && Integer.parseInt(inputDeleteProduct) < prodList.size()){
-                    prodList.remove(Integer.parseInt(inputDeleteProduct) -1);
-                    System.out.println("Итого в списке покупок: " + prodList.size());
-                } else {
-                    System.out.println("Incorrect input");
-                }
+                } else if (inputOperationInt == 4){
+                    searchProduct(scanner, prodList);
 
-            } else if (inputOperationInt == 4){
-                System.out.println("Введите текст для поиска: ");
-                String inputSearchProduct = scanner.nextLine();
-                products.stream()
-                        .filter(searchProduct -> searchProduct.toLowerCase().contains(inputSearchProduct.toLowerCase()))
-                        .forEach(searchProduct -> System.out.println((products.indexOf(searchProduct) +1) + ". " + searchProduct));
-
-            } else if (inputOperationInt == 5){
+                } else if (inputOperationInt == 5){
                 break;
             }
             } catch (NumberFormatException e) {
                 System.out.println("Incorrect input");
             }
         }
+    }
+
+    private static void searchProduct(Scanner scanner, List<String> prodList) {
+        System.out.println("Введите текст для поиска: ");
+        String inputSearchProduct = scanner.nextLine();
+        prodList.stream()
+                .filter(searchProduct -> searchProduct.toLowerCase().contains(inputSearchProduct.toLowerCase()))
+                .forEach(searchProduct -> System.out.println((prodList.indexOf(searchProduct) +1) + ". " + searchProduct));
+    }
+
+    private static void deleteProduct(Scanner scanner, List<String> prodList) {
+        System.out.println("Какую покупку хотите удалить? Введите номер или название");
+        prodList(prodList);
+        String inputDeleteProduct = scanner.nextLine();
+
+        if (prodList.remove(inputDeleteProduct)) {
+            prodList.remove(inputDeleteProduct);
+            System.out.println("Итого в списке покупок: " + prodList.size());
+        } else if (Integer.parseInt(inputDeleteProduct) > 0 && Integer.parseInt(inputDeleteProduct) < prodList.size()){
+            prodList.remove(Integer.parseInt(inputDeleteProduct) -1);
+            System.out.println("Итого в списке покупок: " + prodList.size());
+        } else {
+            System.out.println("Incorrect input");
+        }
+    }
+
+    private static boolean addProduct(Scanner scanner, List<String> prodList, int inputOperationInt) {
+        if (inputOperationInt == 1) {
+            System.out.println("Какую покупку хотите добавить?");
+
+            String inputAddProduct = scanner.nextLine();
+
+                prodList.add(inputAddProduct);
+
+        }
+        return false;
     }
 
     public static <T> void prodList(List<T> list){
